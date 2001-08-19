08ede4b72c81fc2ce61050a611cd4431408c94cb
Morten Brix Pedersen <morten@wtf.dk>
2001-08-19 02:15:17 +0000

    Small developer script for benchmarking speed - read the comments in the script

---
+++ speedtest.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This is a small, but very useful script for finding out where to speedup a
+# Perl program, it will generate a fine statistics on which functions used
+# the most time, and how many calls which were made to it.
+#
+# I hope someone will use this and hopefully make pisg a bit faster :)
+
+perl -d:DProf pisg.pl
+
+dprofpp -u
+
