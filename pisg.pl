4609c6200c2c3dea9040dc5d70062e6ede9bbb56
doomshammer <doomshammer>
2001-08-20 11:07:21 +0000

    Added htmlentities() to $randomline-output... looked really ugly before

--- pisg.pl
+++ pisg.pl
@@ -1286,7 +1286,7 @@ sub activenicks
         "<td bgcolor=\"#$col_r$col_g$col_b\">".sprintf("%.1f",$ch/$line)."</td>"
         : "")
         ."<td bgcolor=\"#$col_r$col_g$col_b\">");
-        html("\"$randomline\"</td>");
+        html("\"" . htmlentities($randomline) . "\"</td>");
 
         if ($users->{userpics}{$nick}) {
             html("<td bgcolor=\"#$col_r$col_g$col_b\" align=\"center\"><img valign=\"middle\" src=\"$conf->{imagepath}$users->{userpics}{$nick}\"></td>");
