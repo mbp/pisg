15ce8d3da0345b228123384c079bd383c3b08715
Morten Brix Pedersen <morten@wtf.dk>
2001-11-19 07:55:08 +0000

    zcbot format by Mo-Ize <mo-ize@pas-tres.net>

---
+++ modules/Pisg/Parser/Format/zcbot.pm
@@ -0,0 +1,100 @@
+package Pisg::Parser::Format::zcbot;
+
+# Documentation for the Pisg::Parser::Format modules is found in Template.pm
+
+use strict;
+$^W = 1;
+
+sub new
+{
+    my ($type, %args) = @_;
+    my $self = {
+        cfg => $args{cfg},
+        debug => $args{debug},
+        normalline => '^[^ ]+ (\d+):[^ ]+ :([^!]+)[^ ]+ PRIVMSG (\#[^ ]+) :[^\001](.*)',
+        actionline => '^[^ ]+ (\d+):[^ ]+ :([^!]+)[^ ]+ PRIVMSG (\#[^ ]+) :\001ACTION (.*)',
+        thirdline  => '^[^ ]+ (\d+):(\d+):\d+ :([^!]+)[^ ]+ ([A-Z]+) (.*)',
+    };
+
+    bless($self, $type);
+    return $self;
+}
+
+sub normalline
+{
+    my ($self, $line, $lines) = @_;
+    my %hash;
+
+    if ($line =~ /$self->{normalline}/ && lc($3) eq lc($self->{cfg}->{channel})) {
+        $self->{debug}->("[$lines] Normal: $1 $2 $3 $4");
+
+        $hash{hour}   = $1;
+        $hash{nick}   = $2;
+        $hash{saying} = $4;
+
+        return \%hash;
+    } else {
+        return;
+    }
+}
+
+sub actionline
+{
+    my ($self, $line, $lines) = @_;
+    my %hash;
+
+    if ($line =~ /$self->{actionline}/ && lc($3) eq lc($self->{cfg}->{channel})) {
+        $self->{debug}->("[$lines] Action: $1 $2 $3 $4");
+
+        $hash{hour}   = $1;
+        $hash{nick}   = $2;
+        $hash{saying} = $4;
+
+        return \%hash;
+    } else {
+        return;
+    }
+}
+
+sub thirdline
+{
+    my ($self, $line, $lines) = @_;
+    my %hash;
+    my $tmp;
+    
+    if ($line =~ /$self->{thirdline}/) {
+        $self->{debug}->("[$lines] ***: $1 $2 $3 $4 $5");
+
+        $hash{hour} = $1;
+        $hash{min}  = $2;
+        $hash{nick} = $3;
+
+        my @arr = split(" ", $5);
+        if ($4 eq 'KICK' && lc($arr[0]) eq lc($self->{cfg}->{channel})) {
+            $hash{kicker} = $hash{nick};
+            $hash{nick} = $arr[1];
+
+        } elsif ($4 eq 'TOPIC' && lc($arr[0]) eq lc($self->{cfg}->{channel})) {
+            $tmp = join(" ", @arr[1..@arr]);
+            $tmp =~ s/^://;
+            $hash{newtopic} = $tmp;
+
+        } elsif ($4 eq 'MODE' && lc($arr[0]) eq lc($self->{cfg}->{channel})) {
+            $hash{newmode} = $arr[1];
+
+        } elsif ($4 eq 'JOIN' && lc($arr[0]) eq ":".lc($self->{cfg}->{channel})) {
+            $hash{newjoin} = $3;
+
+        } elsif ($4 eq 'NICK') {
+            $arr[0] =~ s/^://;
+            $hash{newnick} = $arr[0];
+        }
+
+        return \%hash;
+
+    } else {
+        return;
+    }
+}
+
+1;
