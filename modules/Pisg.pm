1027bc1ef6bc6343b432090ca9e6b03f7f0f448f
Morten Brix Pedersen <morten@wtf.dk>
2001-12-10 19:49:45 +0000

    Bump version number

--- modules/Pisg.pm
+++ modules/Pisg.pm
@@ -202,7 +202,7 @@ sub get_default_config_settings
 
         debug => 0,
         debugfile => 'debug.log',
-        version => "v0.32-cvs",
+        version => "v0.33-cvs",
     };
 
     # Parse the optional overriden configuration variables
