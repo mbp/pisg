066c2c00d4e414f220420d8c850691df184d2bba
Morten Brix Pedersen <morten@wtf.dk>
2001-12-11 22:28:48 +0000

    Add support for Trillian logfiles (thanks to Azhrarn@darkgalaxy.com)

---
+++ modules/Pisg/Parser/Format/Trillian.pm
@@ -0,0 +1,110 @@
+package Pisg::Parser::Format::Trillian;
+
+# Documentation for the Pisg::Parser::Format modules is found in Template.pm
+
+use strict;
+$^W = 1;
+
+sub new
+{
+    my ($type, %args) = @_;
+    my $self = {
+        cfg => $args{cfg},
+        debug => $args{debug},
+        normalline => '^\[(\d+):\d+[^ ]+ <([^>]+)> (.*)',
+        actionline => '^\[(\d+):\d+[^ ]+ \* (\S+) (.*)',
+        thirdline  => '^\[(\d+):(\d+)[^ ]+ \*{3}\s(.+)',
+    };
+
+    bless($self, $type);
+    return $self;
+}
+
+sub normalline
+{
+    my ($self, $line, $lines) = @_;
+    my %hash;
+
+    if ($line =~ /$self->{normalline}/o) {
+        $self->{debug}->("[$lines] Normal: $1 $2 $3");
+
+        $hash{hour}   = $1;
+        $hash{nick}   = remove_prefix($2);
+        $hash{saying} = $3;
+
+        return \%hash;
+    } else {
+        return;
+    }
+}
+
+sub actionline
+{
+    my ($self, $line, $lines) = @_;
+    my %hash;
+
+    if ($line =~ /$self->{actionline}/o) {
+        $self->{debug}->("[$lines] Action: $1 $2 $3");
+
+        $hash{hour}   = $1;
+        $hash{nick}   = remove_prefix($2);
+        $hash{saying} = $3;
+
+        return \%hash;
+    } else {
+        return;
+    }
+}
+
+sub thirdline
+{
+    my ($self, $line, $lines) = @_;
+    my %hash;
+
+    if ($line =~ /$self->{thirdline}/o) {
+            $self->{debug}->("[$lines] ***: $1 $2 $3");
+
+        $hash{hour} = $1;
+        $hash{min}  = $2;
+        $hash{nick} = remove_prefix($3);
+
+        if ($3 =~ /^(\S+) has been kicked off channel (\S+) by (\S+) .+/) {
+            $hash{nick} = remove_prefix($1);
+            $hash{kicker} = $3;
+
+        } elsif ($3 =~ /^(\S+) has changed the topic on channel (\S+) to (.+)/) {
+             $hash{nick} = remove_prefix($1);
+             $hash{newtopic} = $3;
+
+        } elsif ($3 =~ /^Mode change \"(\S+) ([^\"]+)\" .+/) {
+             $hash{nick} = remove_prefix($2);
+             $hash{newmode} = $1;
+
+        } elsif ($3 =~ /^(\S+) \S+ has joined channel \S+/) {
+            $hash{nick} = $1;
+            $hash{newjoin} = $1;
+
+        } elsif ($3 =~ /^(\S+) is now known as (\S+)/) {
+            $hash{nick} = remove_prefix($1);
+            $hash{newnick} = $2;
+        }
+
+        return \%hash;
+
+    } else {
+        return;
+    }
+}
+
+sub remove_prefix
+{
+    my $str = shift;
+
+    $str =~ s/^@//;
+    $str =~ s/^\+//;
+
+    return $str;
+
+}
+
+1;
