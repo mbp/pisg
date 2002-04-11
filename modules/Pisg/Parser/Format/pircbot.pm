6dc5d7c78692deefcfab222a43dc22ac9a36de8c
sbingner <sbingner>
2002-04-11 08:46:50 +0000

    Remove pointless comments I forgot to kill

--- modules/Pisg/Parser/Format/pircbot.pm
+++ modules/Pisg/Parser/Format/pircbot.pm
@@ -1,18 +1,9 @@
-# This is a template for creating your own logfile parser. You can also look
-# in the other .pm files in this directory as good examples.
-
 package Pisg::Parser::Format::pircbot;
+
+# Documentation for the Pisg::Parser::Format modules is found in Template.pm
 
 use strict;
 $^W = 1;
-
-# The 3 variables in the new subrountine, 'normalline', 'actionline' and
-# 'thirdline' represents regular expressions for extracting information from
-# the logfile. normalline is for lines where the person merely said
-# something, actionline is for lines where the person performed an action,
-# and thirdline matches everything else, including things like kicks, nick
-# changes, and op grants.  See the thirdline subroutine for a list of
-# everything it should match.
 
 sub new
 {
@@ -36,7 +27,6 @@ sub new
     return $self;
 }
 
-# Parse a normal line - returns a hash with 'hour', 'nick' and 'saying'
 sub normalline
 {
     my ($self, $line, $lines) = @_;
@@ -68,7 +58,6 @@ sub normalline
     }
 }
 
-# Parse an action line - returns a hash with 'hour', 'nick' and 'saying'
 sub actionline
 {
     my ($self, $line, $lines) = @_;
@@ -100,21 +89,6 @@ sub actionline
     }
 }
 
-# Parses the 'third' line - (the third line is everything else, like
-# topic changes, mode changes, kicks, etc.)
-# thirdline() has to return a hash with the following keys, for
-# every format:
-#   hour            - the hour we're in (for timestamp logging)
-#   min             - the minute we're in (for timestamp logging)
-#   nick            - the nick
-#   kicker          - the nick which kicked somebody (if any)
-#   newtopic        - the new topic (if any)
-#   newmode         - deops or ops, must be '+o' or '-o', or '+ooo'
-#   newjoin         - a new nick which has joined the channel
-#   newnick         - a person has changed nick and this is the new nick
-#
-# The hash may also have a "repeated" key indicating the number of times
-# the line was repeated. (Used by eggdrops log for example.)
 sub thirdline
 {
     my ($self, $line, $lines) = @_;
