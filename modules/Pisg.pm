a3bdaf0f01842038466cdfc16fd49ac833e4125b
Morten Brix Pedersen <morten@wtf.dk>
2002-04-25 19:39:04 +0000

    Bump version

--- modules/Pisg.pm
+++ modules/Pisg.pm
@@ -175,7 +175,7 @@ sub get_default_config_settings
 
         # Developer stuff
 
-        version => "0.38-cvs",
+        version => "0.39-cvs",
     };
 
     # Backwards compatibility with old option names:
