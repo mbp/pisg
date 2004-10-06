bd4723297f872cb2f0c6807eff3a0949b04e8c22
Christoph Berg <cb@df7cb.de>
2004-10-06 22:31:04 +0000

    scripts/eggdrop-pisg.tcl

---
+++ scripts/eggdrop-pisg.tcl
@@ -0,0 +1,40 @@
+#pisg.tcl v0.15 by HM2K - auto stats script for pisg (perl irc statistics generator)
+#based on a script by Arganan
+set pisgver "0.15"
+
+#Location of pisg execuitable perl script
+set pisgexe "/home/nf/pisg/pisg"
+
+#URL of the generated stats
+set pisgurl "http://stats.nemesisforce.com/"
+
+#channel that the stats are generated for
+set pisgchan "#nemesisforce"
+
+#Users with these flags can operate this function
+set pisgflags "nm"
+
+#How often the stats will be updated in minutes, ie: 30 - stats will be updated every 30 minutes
+set pisgtime "30"
+
+bind pub $pisgflags !stats pub:pisgcmd
+
+proc pub:pisgcmd {nick host hand chan arg} {
+	global pisgexe pisgurl pisgchan
+	append out "PRIVMSG $pisgchan :" ; if {[catch {exec $pisgexe} error]} { append out "$pisgexe an error occured: [string totitle $error]" } else { append out "Stats Updated: $pisgurl" }
+	puthelp $out
+}
+
+proc pisgcmd_timer {} {
+	global pisgexe pisgurl pisgchan pisgtime
+	append out "PRIVMSG $pisgchan :" ; if {[catch {exec $pisgexe} error]} { append out "$pisgexe an error occured: [string totitle $error]" } else { append out "Stats Updated: $pisgurl" }
+	puthelp $out
+	timer $pisgtime pisgcmd_timer
+}
+
+if {![info exists {pisgset}]} {
+  set pisgset 1
+  timer 2 pisgcmd_timer
+}
+
+putlog "pisg.tcl $pisgver loaded"
