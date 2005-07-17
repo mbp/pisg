6e9df52e180a0d1f548865caf3800f62525a0d6b
Christoph Berg <cb@df7cb.de>
2005-07-17 22:14:51 +0000

    Change DTD to XHTML 1.1 Strict

--- modules/Pisg/HTMLGenerator.pm
+++ modules/Pisg/HTMLGenerator.pm
@@ -226,7 +226,8 @@ sub _htmlheader
     my $title = $self->_template_text('pagetitle1', %hash);
     if($self->{cfg}->{colorscheme} ne "none") {
         print OUTPUT <<HTML;
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 Strict//EN" 
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=$self->{cfg}->{charset}" />
