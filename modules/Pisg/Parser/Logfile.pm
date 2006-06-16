4b1a0af65de520519a26420e310241e25281aa5a
Christoph Berg <cb@df7cb.de>
2006-06-16 12:47:08 +0000

    Add patch by Teemu Koskinen to speed up karma parsing (SF #1489060).

--- modules/Pisg/Parser/Logfile.pm
+++ modules/Pisg/Parser/Logfile.pm
@@ -414,11 +414,11 @@ sub _parse_file
                     }
 
                     # require 2 chars (catches C++), nick must not end in [+=-]
-                    if ($saying =~ /^(\S*\w\S*\w\S*(?<![+=-]))(\+\+|==|--)$/o) {
+                    if ($saying =~ /^(\S+[^\s+=-])(\+\+|==|--)$/) {
                         my $thing = lc $1;
                         my $k = $2 eq "++" ? 1 : ($2 eq "==" ? 0 : -1);
                         $stats->{karma}{$thing}{$nick} = $k
-                            if !is_ignored($thing) and $thing ne lc($nick);
+                            if $thing =~ /\w\W*?\w/ and !is_ignored($thing) and $thing ne lc($nick);
                     }
 
                     # Find URLs
